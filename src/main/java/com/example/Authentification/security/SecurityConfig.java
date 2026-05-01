14be43b1e8a8bac7520f520f62cac6c6129feec8
Malak <malikazerrouk77@gmail.com>
2026-05-01 19:15:48 +0100

    Add register service with docker setup

--- src/main/java/com/example/Authentification/security/SecurityConfig.java
+++ src/main/java/com/example/Authentification/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.example.Authentification;
+package com.example.Authentification.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
